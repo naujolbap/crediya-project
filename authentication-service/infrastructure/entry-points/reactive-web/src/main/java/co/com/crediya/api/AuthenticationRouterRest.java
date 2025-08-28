9f0b1ebe73d2eaae8996bbc8bbfd0a5a7ecf6b71
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-27 23:06:58 -0500

    Update AuthenticationRouterRest

--- authentication-service/infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/AuthenticationRouterRest.java
+++ authentication-service/infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/AuthenticationRouterRest.java
@@ -1,19 +1,23 @@
 package co.com.crediya.api;
 
+import co.com.crediya.api.config.UserRoutePath;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+
 @Configuration
+@RequiredArgsConstructor
 public class AuthenticationRouterRest {
+
+    private final UserRoutePath userRoutePath;
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction(AuthenticationHandlerV1 authenticationHandlerV1) {
-        return RouterFunctions.route()
-                .path("/api/v1",
-                        builder -> builder.POST("/usuarios", authenticationHandlerV1::listenSaveUser))
-                .build();
+        return RouterFunctions.route(POST(userRoutePath.users()), authenticationHandlerV1::listenSaveUser);
     }
 }
