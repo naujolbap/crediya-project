9c12240f022457fa82f4ddb3a26e20c501a542a4
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-27 22:36:00 -0500

    Update UserUseCase

--- authentication-service/domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
+++ authentication-service/domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
@@ -13,8 +13,4 @@ public class UserUseCase {
     public Mono<User> saveUser(User user) {
         return userRepository.save(user);
     }
-
-    public Mono<User> findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
 }
