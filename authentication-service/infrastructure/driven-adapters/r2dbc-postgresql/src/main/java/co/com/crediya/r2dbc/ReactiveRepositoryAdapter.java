def7d4e0c0e032c0f30295b50c753847f4183931
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-27 23:36:51 -0500

    Update ReactiveRepositoryAdapter

--- authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/ReactiveRepositoryAdapter.java
+++ authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/ReactiveRepositoryAdapter.java
@@ -5,12 +5,22 @@ import co.com.crediya.model.user.gateways.UserRepository;
 import co.com.crediya.r2dbc.entity.UserEntity;
 import co.com.crediya.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class ReactiveRepositoryAdapter extends ReactiveAdapterOperations<User, UserEntity, String, ReactiveRepository> implements UserRepository {
 
     public ReactiveRepositoryAdapter(ReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, userEntity -> mapper.map(userEntity, User.class));
+    }
+
+
+    @Override
+    public Mono<User> findByEmail(String email) {
+        var userEntity = new UserEntity();
+        userEntity.setEmail(email);
+        return repository.findOne(Example.of(userEntity)).map(this::toEntity);
     }
 }
