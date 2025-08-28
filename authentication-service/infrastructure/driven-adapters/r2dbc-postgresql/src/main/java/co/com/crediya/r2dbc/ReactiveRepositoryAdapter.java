b416619b5a5c67ba275d7344ce3203c2203bc900
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-27 23:22:32 -0500

    Update ReactiveRepositoryAdapter

--- authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/ReactiveRepositoryAdapter.java
+++ authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/ReactiveRepositoryAdapter.java
@@ -1,13 +1,14 @@
 package co.com.crediya.r2dbc;
 
 import co.com.crediya.model.user.User;
+import co.com.crediya.model.user.gateways.UserRepository;
 import co.com.crediya.r2dbc.entity.UserEntity;
 import co.com.crediya.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ReactiveRepositoryAdapter extends ReactiveAdapterOperations<User, UserEntity, String, ReactiveRepository> {
+public class ReactiveRepositoryAdapter extends ReactiveAdapterOperations<User, UserEntity, String, ReactiveRepository> implements UserRepository {
 
     public ReactiveRepositoryAdapter(ReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, userEntity -> mapper.map(userEntity, User.class));
