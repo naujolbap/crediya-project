11dbc3498ec2cebb523328f6560280a6639bea85
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-27 22:27:26 -0500

    Update UserUseCase

--- authentication-service/domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
+++ authentication-service/domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
@@ -1,7 +1,20 @@
 package co.com.crediya.usecase.user;
 
+import co.com.crediya.model.user.User;
+import co.com.crediya.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class UserUseCase {
+
+    private final UserRepository userRepository;
+
+    public Mono<User> saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Mono<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
