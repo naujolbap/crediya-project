a6c831be27a5464738669a765aee8d65bd75b065
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-28 21:32:53 -0500

    Add findUserByEmail() in UserReactiveRepository

--- authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepository.java
+++ authentication-service/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/UserReactiveRepository.java
@@ -3,9 +3,12 @@ package co.com.crediya.r2dbc;
 import co.com.crediya.r2dbc.entity.UserEntity;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserReactiveRepository extends ReactiveCrudRepository<UserEntity, UUID>, ReactiveQueryByExampleExecutor<UserEntity> {
 
+    Optional<Mono<UserEntity>> findUserByEmail(String email);
 }
