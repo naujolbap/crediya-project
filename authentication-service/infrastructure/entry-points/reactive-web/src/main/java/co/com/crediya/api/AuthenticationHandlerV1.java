233b94312fc76721201ca50b61fe9341bfbc5ffa
Juan Pablo V. Riaño <naujolbapdev@gmail.com>
2025-08-31 17:00:27 -0500

    Add GlobalExceptionHandler

--- authentication-service/infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/AuthenticationHandlerV1.java
+++ authentication-service/infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/AuthenticationHandlerV1.java
@@ -1,15 +1,11 @@
 package co.com.crediya.api;
 
-import co.com.crediya.api.dto.ErrorResponseDTO;
 import co.com.crediya.dto.UserRequestDTO;
 import co.com.crediya.model.user.User;
-import co.com.crediya.r2dbc.exception.UserAlreadyExistsException;
 import co.com.crediya.usecase.user.UserUseCase;
 import co.com.crediya.uservalidation.UserValidationHelper;
-import co.com.crediya.uservalidation.exception.UserValidationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -31,32 +27,12 @@ public class AuthenticationHandlerV1 {
                 .flatMap(userValidationHelper::validate)
                 .map(dto -> objectMapper.convertValue(dto, User.class))
                 .flatMap(userUseCase::saveUser)
-                .flatMap(this::buildSuccessResponse)
-                .onErrorResume(this::handleError);
+                .flatMap(this::buildSuccessResponse);
     }
 
     private Mono<ServerResponse> buildSuccessResponse(User savedUser) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(savedUser);
-    }
-
-    private Mono<ServerResponse> handleError(Throwable error) {
-        if (error instanceof UserValidationException) {
-            return ServerResponse.badRequest()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(new ErrorResponseDTO("VALIDATION_ERROR", error.getMessage()));
-        }
-
-        if (error instanceof UserAlreadyExistsException) {
-            return ServerResponse.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(new ErrorResponseDTO("USER_ALREADY_EXISTS", error.getMessage()));
-        }
-
-        // Manejar otros tipos de errores
-        return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(new ErrorResponseDTO("INTERNAL_ERROR", error.getMessage()));
     }
 }
